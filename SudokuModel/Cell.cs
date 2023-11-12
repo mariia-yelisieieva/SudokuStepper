df7cf625d2f6b318c5bcd2502400162a6d7f90b5
MariiaYelisieieva <11693026+mariia-yelisieieva@users.noreply.github.com>
2023-11-12 11:02:44 +0100

    Add project files.

---
+++ SudokuModel/Cell.cs
@@ -0,0 +1,50 @@
+﻿namespace SudokuModel
+{
+    internal class Cell
+    {
+        private readonly byte X;
+        private readonly byte Y;
+        public Cell(byte x, byte y)
+        {
+            if (x < 0 || x > 8)
+                throw new ArgumentException($"{x} coordinate is not supported");
+            if (y < 0 || y > 8)
+                throw new ArgumentException($"{y} coordinate is not supported");
+            X = x;
+            Y = y;
+            Answered = false;
+        }
+
+        public bool Answered { get; set; } = false;
+        private byte value = 0;
+        public byte Value
+        {
+            get => value;
+            set
+            {
+                Answered = true; 
+                this.value = value;
+            }
+        }
+
+        private readonly byte[] suggestions = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public byte[] GetSuggestions()
+        {
+            byte[] copy = new byte[9];
+            Array.Copy(suggestions, copy, suggestions.Length);
+            return copy;
+        }
+        public void AddSuggestion(byte suggestedNumber)
+        {
+            if (suggestedNumber < 1 || suggestedNumber > 9)
+                throw new ArgumentException($"suggested number is out of range, cell {X},{Y}");
+            suggestions[suggestedNumber - 1] = suggestedNumber;
+        }
+        public void RemoveSuggestion(byte suggestedNumber)
+        {
+            if (suggestedNumber < 1 || suggestedNumber > 9)
+                throw new ArgumentException($"suggested number is out of range, cell {X},{Y}");
+            suggestions[suggestedNumber - 1] = suggestedNumber;
+        }
+    }
+}
---
+++ SudokuModel/Grid.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuModel
+{   
+    public class Grid
+    {
+        private readonly Cell[] cells = new Cell[81];
+
+        private readonly byte[] cellValues;
+        public Grid(params byte[] cellValues)
+        {
+            if (cellValues.Length != 81)
+                throw new ArgumentException();
+            this.cellValues = cellValues;
+
+            for (byte x = 0; x < 9; x++)
+                for (byte y = 0; y < 9; y++)
+                    cells[y * 9 + x] = new Cell(x, y);
+            
+            for (int i = 0; i < cellValues.Length; i++)
+            {
+                if (cellValues[i] == 0)
+                    continue;
+                cells[i].Value = cellValues[i];
+            }
+        }
+
+        public void Answer(byte x, byte y, byte value)
+        {
+            if (x < 0 || x > 8)
+                throw new ArgumentException($"{x} coordinate is not supported");
+            if (y < 0 || y > 8)
+                throw new ArgumentException($"{y} coordinate is not supported");
+            if (value < 1 || value > 9)
+                throw new ArgumentException($"{value} value is not supported");
+
+            cells[y * 9 + x].Value = value;
+        }
+
+        public void AddSuggestion(byte x, byte y, byte value)
+        {
+            if (x < 0 || x > 8)
+                throw new ArgumentException($"{x} coordinate is not supported");
+            if (y < 0 || y > 8)
+                throw new ArgumentException($"{y} coordinate is not supported");
+            if (value < 1 || value > 9)
+                throw new ArgumentException($"{value} value is not supported");
+
+            cells[y * 9 + x].AddSuggestion(value);
+        }
+
+        public void RemoveSuggestion(byte x, byte y, byte value)
+        {
+            if (x < 0 || x > 8)
+                throw new ArgumentException($"{x} coordinate is not supported");
+            if (y < 0 || y > 8)
+                throw new ArgumentException($"{y} coordinate is not supported");
+            if (value < 1 || value > 9)
+                throw new ArgumentException($"{value} value is not supported");
+
+            cells[y * 9 + x].RemoveSuggestion(value);
+        }
+
+        //public Grid Copy()
+        //{
+        //    var copy = new Grid(cellValues);
+        //    return copy;
+        //}
+    }
+}
